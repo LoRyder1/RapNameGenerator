d826078e55a02d8c623ecb1b1d853e72c9a17851
Lovinder Pnag <lspnag@gmail.com>
2015-05-31 16:31:21 -0400

    working rap name generator

--- js/script.js
+++ js/script.js
@@ -20,13 +20,47 @@ function Generator() {
 
 }
 
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+String.prototype.acronym = function() {
+    return this.toUpperCase().split('').join('.') +'.';
+}
 
 //Add your codez here
 
-
 $(document).ready(function() {
 
-    var engine = new Generator;
-    //Add your codez here
+  var engine = new Generator;
+  //Add your codez here
+
+  $("#enter").on("click", function() {
+
+    var user_first_name = $("#user-input").val().trim().capitalizeFirstLetter();
+
+    var rap_names = []
+
+    var rap_fn1 = user_first_name + " " + _.sample(engine.last_names);
+    var rap_fn2 = user_first_name.charAt(0) + " " + _.sample(engine.last_names);
+    var rap_fn3 = user_first_name.acronym() + " " + _.sample(engine.last_names)
+    var rap_ln1 = _.sample(engine.first_names) + " " + user_first_name;
+    var rap_ln2 = _.sample(engine.first_names) + " " + user_first_name.charAt(0);
+    var rap_ln3 = _.sample(engine.first_names) + " " + user_first_name.acronym();
+
+    rap_names.push(rap_fn1,rap_fn2,rap_fn3,rap_ln1,rap_ln2,rap_ln3)
+
+    // console.log(_.sample(rap_names));
+    if (!user_first_name.length){
+      $('.response').hide();
+      $('.error').show();
+    } else {
+      $('.error').hide();
+      $('.response').text(_.sample(rap_names))
+      $('.response').show();
+    }
+
+
+  });
 
 });
