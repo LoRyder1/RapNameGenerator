c809bc78a3cc0df648dc7d8ad9940efe80413181
LoRyder1 <lpnag@CMDVMOSXLT075.local>
2015-06-01 18:15:37 -0400

    extracted out make rap names to ouside function

--- js/script.js
+++ js/script.js
@@ -13,11 +13,9 @@
  **/
 
 function Generator() {
-
     /* Name Arrays: Customize names to change possible output */
     this.last_names = ['the Chef', 'Digital', 'Wise', 'Knight', 'Wrecka', 'the Genius', 'the Zoo Keeper', 'the Monk', 'the Scientist', 'the Disciple', 'the Darkman', 'Pellegrino', 'the Ill Figure', 'Rocks The World', 'the Baptist',];
     this.first_names = ['Inspectah', 'Masta', 'Poppa', 'Five Foot', 'Ghostface', 'Old Dirty'];
-
 }
 
 String.prototype.capitalizeFirstLetter = function() {
@@ -28,39 +26,41 @@ String.prototype.acronym = function() {
     return this.toUpperCase().split('').join('.') +'.';
 }
 
-//Add your codez here
+
+Generator.prototype.makeRapNames = function(input) {
+  var rap_names = [];
+
+  var rap_fn1 = input + " " + _.sample(this.last_names);
+  var rap_fn2 = input.charAt(0) + " " + _.sample(this.last_names);
+  var rap_fn3 = input.acronym() + " " + _.sample(this.last_names)
+  var rap_ln1 = _.sample(this.first_names) + " " + input;
+  var rap_ln2 = _.sample(this.first_names) + " " + input.charAt(0);
+  var rap_ln3 = _.sample(this.first_names) + " " + input.acronym();
+
+  rap_names.push(rap_fn1,rap_fn2,rap_fn3,rap_ln1,rap_ln2,rap_ln3)
+
+  return rap_names; 
+}
+
+// console.log(engine.makeRapNames("hello"))
 
 $(document).ready(function() {
 
   var engine = new Generator;
-  //Add your codez here
 
   $("#enter").on("click", function() {
 
     var user_first_name = $("#user-input").val().trim().capitalizeFirstLetter();
 
-    var rap_names = []
+    // console.log(_.sample(engine2.makeRapNames(user_first_name)));
 
-    var rap_fn1 = user_first_name + " " + _.sample(engine.last_names);
-    var rap_fn2 = user_first_name.charAt(0) + " " + _.sample(engine.last_names);
-    var rap_fn3 = user_first_name.acronym() + " " + _.sample(engine.last_names)
-    var rap_ln1 = _.sample(engine.first_names) + " " + user_first_name;
-    var rap_ln2 = _.sample(engine.first_names) + " " + user_first_name.charAt(0);
-    var rap_ln3 = _.sample(engine.first_names) + " " + user_first_name.acronym();
-
-    rap_names.push(rap_fn1,rap_fn2,rap_fn3,rap_ln1,rap_ln2,rap_ln3)
-
-    // console.log(_.sample(rap_names));
     if (!user_first_name.length){
       $('.response').hide();
       $('.error').show();
     } else {
       $('.error').hide();
-      $('.response').text(_.sample(rap_names))
+      $('.response').text(_.sample(engine.makeRapNames(user_first_name)))
       $('.response').show();
     }
-
-
   });
-
 });
